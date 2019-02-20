00fa1e9a847cebffb411eb4b05e89ea7d382905d
iondo <ines.ondo.baka@gmail.com>
2019-02-20 22:10:00 +0100

    update box

--- routes/moves.js
+++ routes/moves.js
@@ -63,6 +63,31 @@ router.get('/:moveId/box/:boxId', (req, res, next) => {
     });
 });
 
+router.get('/:moveId/box/:boxId/update', (req, res, next) => {
+  const { boxId } = req.params;
+  Box.findById(boxId)
+    .then((box) => {
+      res.render('box-update', {
+        box,
+      });
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.post('/:moveId/box/:boxId/update', (req, res, next) => {
+  const { moveId, boxId } = req.params;
+  const { name, description } = req.body;
+  Box.findByIdAndUpdate(boxId, { name, description })
+    .then(() => {
+      res.redirect(`/move/${moveId}/box/${boxId}`);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
 router.get('/:id/update', (req, res, next) => {
   const { id } = req.params;
   Move.findById(id)
